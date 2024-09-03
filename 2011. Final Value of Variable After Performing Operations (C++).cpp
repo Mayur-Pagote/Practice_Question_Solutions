8169d0802d41f472f5d1590e955a15433fcce7e7
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-03 18:56:28 +0530

    Solution of 2011. Final Value of Variable After Performing Operations

---
+++ 2011. Final Value of Variable After Performing Operations (C++).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+
+        int length = operations.size(); // This variable will store length of operations array
+
+        int x = 0; // Initializing x with 0
+
+        // Loop to compare operations
+        for (int i = 0; i < length; i++){
+            if (operations[i] == "--X") x -= 1;
+            else if (operations[i] == "X--") x -= 1;
+            else if (operations[i] == "++X") x += 1;
+            else if (operations[i] == "X++") x += 1;
+        };
+        
+    return x;
+    }      
+};
