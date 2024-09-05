2961cf6313b3279ff78a57ee28662c07b81a016d
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-05 18:04:15 +0530

    Update 1929. Concatenation of Array (Python).py

--- 1929. Concatenation of Array (Python).py
+++ 1929. Concatenation of Array (Python).py
@@ -1,4 +1,4 @@
 class Solution(object):
     def getConcatenation(self, nums):
-        temp = nums
-        return temp+nums
+        temp = nums # Initializing temp variable with nums
+        return temp+nums # Concatenating both the list and returning it
