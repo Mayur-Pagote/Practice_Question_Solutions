aa7a579a9f762d793878890f8f3ccf6979145388
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-08 16:26:31 +0530

    Update 9. Palindrome Number (C#).cs

--- 9. Palindrome Number (C#).cs
+++ 9. Palindrome Number (C#).cs
@@ -1,10 +1,11 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        if (x<0) return false;
+        if (x<0) return false; // This will check given number is negative or not
         
         long num = 0;
         long temp = x;
-        while(x>0){
+        
+        while(x>0){ // This will reverse the given number
             long rem = x%10;
             num = num*10 + rem;
             x = x/10;
