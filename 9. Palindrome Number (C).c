7bc249b9a477f550a65b1afbf45158e8979cfee5
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-08 16:23:06 +0530

    Create 9. Palindrome Number (C).c

---
+++ 9. Palindrome Number (C).c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    if (x<0) return false; // This will check given number is negative or not
+        
+        long num = 0;
+        long temp = x;
+  
+        while(x>0){ // This will reverse the given number
+            long rem = x%10;
+            num = num*10 + rem;
+            x = x/10;
+        }
+        return temp == num;
+    
+}
