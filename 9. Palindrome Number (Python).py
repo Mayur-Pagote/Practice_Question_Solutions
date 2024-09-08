792b1b12050d573bd70014c23bdf7eab5e89d795
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-08 16:12:06 +0530

    Create 9. Palindrome Number (Python).py

---
+++ 9. Palindrome Number (Python).py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        
+        if x == 0: # This will handle specific test case of 0
+            return True
+          
+        elif (x < 0) or (x % 10 == 0): # This will check given number is negative or mod of 10
+            return False
+          
+        else: 
+            reverse = 0
+            temp = x
+            while(x>0): # This will help in reversing the given number
+                rem = x%10
+                reverse = reverse*10 + rem
+                x = x//10
+        
+        return temp == reverse
