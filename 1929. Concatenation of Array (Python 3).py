7889369013c89673ad39cc2e4a2ae3f55dba616c
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-10 18:09:26 +0530

    Create 1929. Concatenation of Array (Python 3).py

---
+++ 1929. Concatenation of Array (Python 3).py
@@ -0,0 +1,4 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        temp = nums # Initializing temp variable with nums
+        return temp+nums # Concatenating both the list and returning it
