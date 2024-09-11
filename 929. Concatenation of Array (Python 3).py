6ca95557613b57da6a07fae2a8ab8a1333d2bf51
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-11 10:15:30 +0530

    Delete 929. Concatenation of Array (Python 3).py

--- 929. Concatenation of Array (Python 3).py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        temp = nums # Initializing temp variable with nums
-        return temp+nums # Concatenating both the list and returning it
