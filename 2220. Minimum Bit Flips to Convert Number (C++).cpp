480296378e6d8bdd21f1d83b8b81b80d8143aeb5
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-11 10:06:28 +0530

    Create 2220. Minimum Bit Flips to Convert Number (C++).cpp

---
+++ 2220. Minimum Bit Flips to Convert Number (C++).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0; // Initialize the count of bit flips required
+
+        // Continue processing until both start and goal are reduced to 0
+        while (start || goal) {
+            // Check if the least significant bit of start differs from the least significant bit of goal
+            if ((start & 1) != (goal & 1)) {
+                count += 1; 
+            }
+
+            // Right shift both start and goal by 1 bit (essentially dividing by 2)
+            start = start / 2; 
+            goal = goal / 2;   
+        }
+
+        return count; 
+    }
+};
