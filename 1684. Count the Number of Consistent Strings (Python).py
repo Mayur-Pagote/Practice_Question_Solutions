5832ec3a9089a3799a333174d036c14ed31d24e1
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-12 17:42:26 +0530

    Create 1684. Count the Number of Consistent Strings (Python).py

---
+++ 1684. Count the Number of Consistent Strings (Python).py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        count = 0
+      
+        # Iterate over each word in the words list
+        for i in words:
+            # Initialize a temporary counter to check if all characters in the word are allowed
+            temp_count = 0
+            # Iterate over each character in the current word
+            for j in i:
+                # Check if the character is in the allowed set
+                if j in allowed:
+                    # Increment the temporary counter if the character is allowed
+                    temp_count += 1
+            
+            # If the temporary counter equals the length of the word, it means all characters in the word are allowed
+            if temp_count == len(i):
+                count += 1
+
+        return count
