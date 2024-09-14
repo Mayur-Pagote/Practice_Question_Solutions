fb1d096e47163398ac0902fc5827e0994fd44382
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-14 17:34:20 +0530

    Create 258. Add Digits (Swift).swift

---
+++ 258. Add Digits (Swift).swift
@@ -0,0 +1,11 @@
+class Solution {
+    func addDigits(_ num: Int) -> Int {
+        // Return the digital root of num using the formula 1 + (num - 1) % 9
+        if (num == 0) {
+            return 0
+        }
+        else {
+            return 1 + (num - 1)%9
+        }
+    }
+}
