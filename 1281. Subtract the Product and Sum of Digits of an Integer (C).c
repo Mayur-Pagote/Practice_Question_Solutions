fcdb2f4e1aa3bfce6328250d97520d0dee21c5e9
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-09-18 09:35:33 +0530

    Create 1281. Subtract the Product and Sum of Digits of an Integer (C).c

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer (C).c
@@ -0,0 +1,15 @@
+int subtractProductAndSum(int n) {
+    int rem = 0;        
+        int sum_n = 0;     
+        int product_n = 1;  
+
+        // Loop until all digits of n are processed
+        while(n > 0) {
+            rem = n % 10;   // Get the last digit of n
+            sum_n += rem;   // Add the digit to the sum
+            product_n *= rem; // Multiply the digit to the product
+            n /= 10;        // Remove the last digit from n
+        }
+        
+        return product_n - sum_n;
+}
