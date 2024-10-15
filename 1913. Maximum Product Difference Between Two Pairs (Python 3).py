b77aedc99ecdf621aa48e1d38dbfb52ab6fd8223
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-10-15 16:19:48 +0530

    Create 1913. Maximum Product Difference Between Two Pairs (Python 3).py

---
+++ 1913. Maximum Product Difference Between Two Pairs (Python 3).py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        # Sort the list of numbers in ascending order
+        nums.sort()
+        
+        # Calculate the product of the two largest numbers
+        max_product = nums[-1] * nums[-2]
+        
+        # Calculate the product of the two smallest numbers
+        min_product = nums[0] * nums[1]
+        
+        return max_product - min_product
