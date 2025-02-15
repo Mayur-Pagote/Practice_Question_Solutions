a3cbf5e65bd963d0231c6f44b98cbf0f80a70b34
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-02-15 08:41:29 +0530

    Update 1281. Subtract the Product and Sum of Digits of an Integer (C++).cpp

--- 1281. Subtract the Product and Sum of Digits of an Integer (C++).cpp
+++ 1281. Subtract the Product and Sum of Digits of an Integer (C++).cpp
@@ -5,12 +5,11 @@ public:
         int sum_n = 0;     
         int product_n = 1;  
 
-        // Loop until all digits of n are processed
         while(n > 0) {
-            rem = n % 10;   // Get the last digit of n
-            sum_n += rem;   // Add the digit to the sum
-            product_n *= rem; // Multiply the digit to the product
-            n /= 10;        // Remove the last digit from n
+            rem = n % 10;  
+            sum_n += rem;   
+            product_n *= rem; 
+            n /= 10;     
         }
         
         return product_n - sum_n;
